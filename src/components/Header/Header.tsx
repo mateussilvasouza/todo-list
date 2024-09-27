a605bc66c34a0275b935e5bb7b00cab032a0dacb
Mateus Souza <mateus.souza@vaifacil.com>
2024-09-26 22:54:05 -0300

    Fix image path

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import style from './Header.module.css'
 export function Header(){ 
   return(
     <header className={style.wrapper}>
-      <img src="src/assets/logo.png" alt="Logo Todo List" />
+      <img src="./src/assets/logo.png" alt="Logo Todo List" />
     </header>
   )
 }
--- src/components/List/Empty/Empty.tsx
+++ src/components/List/Empty/Empty.tsx
@@ -3,7 +3,7 @@ import styles from './Empty.module.css'
 export function Empty(){
   return(
     <div className={styles.container}>
-      <img src="src/assets/clipboard.png" alt="Prancheta" />
+      <img src="./src/assets/clipboard.png" alt="Prancheta" />
       <p>
         <strong>Você não tem tarefas cadastradas</strong>
         Crie tarefas e organize seus itens a fazer
